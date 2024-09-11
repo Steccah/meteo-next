24fc728b7847ef7349cff3c1ad6d23320070e2b3
Steccah <me@stecca.dev>
2024-09-11 19:47:48 +0200

    Add tracking script for website analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,14 +16,16 @@ const geistMono = localFont({
 export const metadata = {
   title: "Stecca meteo",
   description: "Stecca meteo is a simple weather app using openwheather API",
+
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <script defer src="https://t.stecca.dev/script.js" data-website-id="07787f12-bf4b-4a1b-b328-ead15b210688"></script>
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system">
